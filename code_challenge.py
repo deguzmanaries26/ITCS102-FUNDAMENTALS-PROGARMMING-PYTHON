837aa0ef1e0f6e1bc1f99fe04feb175ca2d5372a
deguzmanaries26 <deguzmanaris101@gmail.com>
2025-08-16 18:19:03 +0800

    Update code_challenge1.py

--- code_challenge.py
+++ code_challenge.py
@@ -1,3 +1,2 @@
 name=input("Type your name: ")
-message=(f"hello, ", name)
-print(f"\t\t\t*\n\t\t*\t\t*\n\t*\t\t\t\t*\n*\t\t  hi, " + name + "\t\t*\n\t*\t\t\t\t*\n\t\t*\t\t*\n\t\t\t*")
+print("\t\t\t*\n\n\t\t*\t\t*\n\n\t*\t\t\t\t*\n\n*\t\t  hi, " + name + "\t\t*\n\n\t*\t\t\t\t*\n\n\t\t*\t\t*\n\n\t\t\t*")
