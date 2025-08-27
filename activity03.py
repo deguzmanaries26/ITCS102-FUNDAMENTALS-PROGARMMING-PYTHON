96309992ee4bb88730e3aa7aadc5def1db95d688
deguzmanaries26 <deguzmanaris101@gmail.com>
2025-08-27 17:51:01 +0800

    Update activity03.py

--- activity03.py
+++ activity03.py
@@ -1,2 +1,2 @@
-answer=input("who is the greatest singer? ")
-print("Is that even a question?? You know there's only one right answer, and it's", answer)
+name = input("Identify yourself: ")
+print("Hello", name, "Welcome to the Matrix")
