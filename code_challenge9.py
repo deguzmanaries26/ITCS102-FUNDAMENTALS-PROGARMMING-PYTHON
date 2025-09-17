5032b0515497c61cc057db63de6a1b25a84ce1b5
deguzmanaries26 <deguzmanaris101@gmail.com>
2025-09-17 13:39:33 +0800

    created in onecompiler

---
+++ code_challenge9.py
@@ -0,0 +1,7 @@
+print("⏳COUNTDOWN TIMER SIMULATOR")
+number = int(input("Enter a starting number for the countdown: "))
+print(number)
+print("\n⏳Countdown started:")
+for a in range(number, 0, -1):
+  print(a)
+print("Liftoff!🚀")
